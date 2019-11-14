6f9fb85d28b33414911d6e2bf6aa75dea0565e45
Jesse Hall <jessehall3@gmail.com>
2019-11-13 18:47:10 -0600

    Can view product list.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Hello world</h1>
+    <h1>{{ hi }}</h1>
+    <h1>{{ numbers }}</h1>
+    <ul>
+      <li v-for="n in numbers">{{ n }}</li>
+    </ul>
+    <product-list/>
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList'
+
+export default {
+  name: 'app',
+  components: {
+    'product-list': ProductList,
+  },
+  data: function(){
+    return {
+      hi: "hi",
+      numbers: [ 'one', 'two' ],
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products">
+        {{ product.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      products: [{
+        name: "apple",
+        price: "20"
+      }]
+    }
+  },
+  // created: function(){
+  //   const self = this
+  //   shop.getProducts(products => {
+  //     self.products = products
+  //   })
+  // }
+}
+</script>
