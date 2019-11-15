e2ef70bd93ed6e7b7af0fce668646474194fb69d
Jesse Hall <jessehall3@gmail.com>
2019-11-15 14:11:44 -0600

    Now showing all products and disablying "add to cart" button is out of stock.

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -6,7 +6,11 @@
       <ul>
         <li v-for="product in products">
           {{ product.title }} - {{ product.price | currency }} - {{ product.inventory }}
-          <button @click="addProductToCart(product)">Add To Cart</button>
+          <button
+            @click="addProductToCart(product)"
+            :disabled="product.inventory <= 0">
+            Add To Cart
+          </button>
         </li>
       </ul>
       <p v-if="products.length === 0">
@@ -27,7 +31,7 @@ export default {
   },
   computed: {
     products () {
-      return this.$store.getters.availableProducts
+      return this.$store.state.products
     },
     totalAmountDue () {
       return this.$store.getters.totalAmountDue
